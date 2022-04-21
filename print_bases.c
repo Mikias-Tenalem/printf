e110d71e42dacbc1d1fab988f192d1bba949e93a
Michael-Alem <99313463+Michael-Alem@users.noreply.github.com>
2022-04-21 09:53:25 +0300

    Update print_bases.c

--- print_bases.c
+++ print_bases.c
@@ -3,11 +3,11 @@
 /**
  * print_hex - prints a number in hexadecimal base,
  * in lowercase
- * @l: va_list arguments from _printf
- * @f: pointer to the struct flags that determines
+ * @l: va_list argument from _printf
+ * @f: pointer to the struct flag that determines
  * if a flag is passed to _printf
- * Description: the function calls convert() which in turns converts the input
- * number into the correct base and returns it as a string
+ * Description: the function call convert() which in turn convert the input
+ * number into the correct base and return it as a string
  * Return: the number of char printed
  */
 int print_hex(va_list l, flags_t *f)
@@ -23,13 +23,13 @@ int print_hex(va_list l, flags_t *f)
 }
 
 /**
- * print_hex_big - prints a number in hexadecimal base,
+ * print_hex_big - print a number in hexadecimal base,
  * in uppercase
- * @l: va_list arguments from _printf
- * @f: pointer to the struct that determines
+ * @l: va_list argument from _printf
+ * @f: pointer to the struct that determine
  * if a flag is passed to _printf
- * Description: the function calls convert() which in turns converts the input
- * number into the correct base and returns it as a string
+ * Description: the function call convert() which in turn convert the input
+ * number into the correct base and return it as a string
  * Return: the number of char printed
  */
 int print_hex_big(va_list l, flags_t *f)
@@ -45,11 +45,11 @@ int print_hex_big(va_list l, flags_t *f)
 }
 
 /**
- * print_binary - prints a number in base 2
- * @l: va_list arguments from _printf
- * @f: pointer to the struct that determines
+ * print_binary - print a number in base 2
+ * @l: va_list argument from _printf
+ * @f: pointer to the struct that determine
  * if a flag is passed to _printf
- * Description: the function calls convert() which in turns converts the input
+ * Description: the function call convert() which in turn convert the input
  * number into the correct base and returns it as a string
  * Return: the number of char printed
  */
@@ -63,12 +63,12 @@ int print_binary(va_list l, flags_t *f)
 }
 
 /**
- * print_octal - prints a number in base 8
- * @l: va_list arguments from _printf
- * @f: pointer to the struct that determines
+ * print_octal - print a number in base 8
+ * @l: va_list argument from _printf
+ * @f: pointer to the struct that determine
  * if a flag is passed to _printf
- * Description: the function calls convert() which in turns converts the input
- * number into the correct base and returns it as a string
+ * Description: the function call convert() which in turn convert the input
+ * number into the correct base and return it as a string
  * Return: the number of char printed
  */
 int print_octal(va_list l, flags_t *f)
