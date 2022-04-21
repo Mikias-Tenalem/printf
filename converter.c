4ec45a5f45fe97904603a8b9d5a7200cbedfea70
Michael-Alem <99313463+Michael-Alem@users.noreply.github.com>
2022-04-21 09:44:25 +0300

    Update converter.c

--- converter.c
+++ converter.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * convert - converts number and base into string
+ * convert - converts number and base into a string
  * @num: input number
  * @base: input base
- * @lowercase: flag if hexa values need to be lowercase
- * Return: result string
+ * @lowercase: flag if hexa values needs to be lowercase
+ * Return: result will be string
  */
 char *convert(unsigned long int num, int base, int lowercase)
 {
