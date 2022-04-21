51b6782b4cc2153692b90be8c6aed144741dcdaa
Michael-Alem <99313463+Michael-Alem@users.noreply.github.com>
2022-04-21 09:50:01 +0300

    Update print_address.c

--- print_address.c
+++ print_address.c
@@ -2,8 +2,8 @@
 
 /**
  * print_address - prints address of input in hexa format
- * @l: va_list arguments from _printf
- * @f: pointer to the struct flags that determines
+ * @l: va_list argument from _printf
+ * @f: pointer to the struct flag that determine
  * if a flag is passed to _printf
  * Return: number of char printed
  */
