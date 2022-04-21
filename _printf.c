a20da4320209dd3e1bdb7e892bc42ac14c0ef24a
Michael-Alem <99313463+Michael-Alem@users.noreply.github.com>
2022-04-21 10:17:33 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _printf - prints anything
+ * _printf - print anything
  * @format: the format string
  *
- * Return: number of bytes printed
+ * Return: number of byte printed
  */
 int _printf(const char *format, ...)
 {
